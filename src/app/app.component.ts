5f8f3e46c9e85e7780e8b49417fdef2cc782691c
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-07-25 15:27:19 +0300

    Grafiğin oluşturulması

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
-import * as Highcharts from "highcharts";
+import * as Highcharts from 'highcharts';
+import * as signalR from "@microsoft/signalr";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ChartsClient';
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: 'Başlık',
+    },
+    subtitle: {
+      text: 'Alt Başlık',
+    },
+    yAxis: {
+      title: {
+        text: 'Y ekseni',
+      },
+    },
+    xAxis: {
+      accessibility: {
+        rangeDescription: '2021-2022',
+      },
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+    },
+    series: [
+      {
+        name: 'X',
+        type: 'line',
+        data: [1000, 2000, 3000],
+      },
+      {
+        name: 'Y',
+        type: 'line',
+        data: [9000, 5000, 6000],
+      },
+      {
+        name: 'Z',
+        type: 'line',
+        data: [7000, 6000, 1000],
+      }
+    ],
+    plotOptions:{
+      series:{
+        label:{
+          connectorAllowed:true
+        },
+        pointStart:100
+      }
+    }
+  };
 }
